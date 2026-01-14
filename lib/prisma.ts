c5002798fe44f1f5da67c651a682e114dbbf0fca
xplorekamrul <mdkamruzzaman099826@gmail.com>
2026-01-14 20:30:35 +0600

    prisma 6

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,32 +1,19 @@
-// src/lib/prisma.ts
-
-import { PrismaPg } from '@prisma/adapter-pg'
-import { PrismaClient } from '@prisma/client'
-
-if (!process.env.DATABASE_URL) {
-  throw new Error('DATABASE_URL is not set in environment variables')
-}
-
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL,
-})
+// lib/prisma.ts
+import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = globalThis as unknown as {
-  prisma?: PrismaClient
-}
+  prisma: PrismaClient | undefined;
+};
 
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
-    adapter,
     log:
-      process.env.NODE_ENV === 'development'
-        ? ['query', 'error', 'warn']
-        : ['error', 'warn'],
-  })
+      process.env.NODE_ENV === "development"
+        ? ["query", "error", "warn"]
+        : ["error"],
+  });
 
-if (process.env.NODE_ENV !== 'production') {
-  globalForPrisma.prisma = prisma
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
 }
-
-export default prisma
